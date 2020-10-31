c8aa7833c345755cd5549862e2f7f4db17a2e96b
Niks Hariman <nhariman@hotmail.com>
2020-10-31 19:03:28 +0100

    UP main.c minishell.h ADD srcs/main niks.h

--- main.c
+++ main.c
@@ -6,20 +6,60 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/31 15:07:26 by ybakker       #+#    #+#                 */
-/*   Updated: 2020/10/31 15:35:45 by ybakker       ########   odam.nl         */
+/*   Updated: 2020/10/31 18:53:14 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include srcs/minishell.h
+#include "srcs/minishell.h"
+#include <stdio.h>
 
-int		main(int argc, char **argv)
+/*
+** TODO:
+** open stdin and read from it. (GNL)
+*/
+
+static void	minishell_parser(char *line)
 {
-	char **tmp;
-	int tmp2;
+	int i;
 
-	tmp = argv;
-	tmp2 = argc;
-	return (0);
+	i = 0;
+	ft_printf("%s\n", line);
+	while (line[i] == ' ')
+		i++;
+	if (!ft_strncmp(line, "echo ", ft_strlen("echo ")))
+			exit(0); // replace with echo function
+
 }
 
-//hallo, even een test
+static char	*ft_strlower(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		line[i] = ft_tolower(line[i]);
+		i++;
+	}
+	return (line);
+}
+
+int		main(void)
+{
+	int		fd;
+	int		i;
+	char	*line;
+
+	fd = 0;
+	i = 1;
+	while (i == 1)
+	{
+		i = get_next_line(fd, &line);
+		line = ft_strlower(line);
+		if (!ft_strncmp(line, "exit\n", ft_strlen("exit\n")))
+			exit(0); // replace with an exit command that frees everything.
+		minishell_parser(line); //function that processes the line argument.
+		//free(line);
+	}
+	return (0);
+}
