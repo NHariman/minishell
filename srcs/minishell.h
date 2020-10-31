01927ab3236d7e82ea647d954a50478a91ed3c1f
Niks Hariman <nhariman@hotmail.com>
2020-10-31 16:20:00 +0100

    UPDATED minishell.h

---
+++ srcs/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/31 16:18:06 by nhariman      #+#    #+#                 */
+/*   Updated: 2020/10/31 16:19:25 by nhariman      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "yusha.h"
+#include "niks.h"
+
+#endif
