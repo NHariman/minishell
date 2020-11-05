258fae2a2fe1c6c856436828a6f0ca3d2b3afc0c
Yusha <yushabakker1@gmail.com>
2020-11-05 16:01:29 +0100

    commit, added -n flag, added no str, meaning if you jsut type in something

--- srcs/echo/echo.c
+++ srcs/echo/echo.c
@@ -6,108 +6,177 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/31 21:21:07 by ybakker       #+#    #+#                 */
-/*   Updated: 2020/10/31 22:32:52 by ybakker       ########   odam.nl         */
+/*   Updated: 2020/11/05 15:59:44 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+// ft_printf("ERROR START\n");
+// ft_printf("ERROR END\n");
 
 #include "../minishell.h"
 #include <stdio.h>
 
-int     check_n(char* str)
+int	ft_strncmp_echo(int i, char *s1, char *s2, int n)
 {
-	int i = 0;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '-')
-		i++;
-	else
-		return(0);
-	if (str[i] == 'n')
-	{
-		while (str[i] == 'n')
-			i++;
-	}
-	if (str[i] == ' ')
-		return (1);
-	return (0);
-}
+	int y;
 
-int     lenght_n(char* str)
-{
-	int i = 0;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '-')
-		i++;
-	else
+	y = 0;
+	if (n == 0)
 		return (0);
-	if (str[i] == 'n')
+	while (n != 0)
 	{
-		while (str[i] == 'n')
-			i++;
+		if (s1[i] != s2[y])
+			return (i - y);//if wrong, then return the original i
+		i++;
+		y++;
+		n--;
 	}
-	if (str[i] == ' ')
-		return (i);
-	return (0);
+	return (i);
 }
 
-void    no_str(t_struct_m *Echo, char* str)
+int		check_flag_n(char *str)
 {
-	int     y   =   Echo->i;
-	int     len =   0;
-	char*   tmp =   NULL;
+	int i;
 
-	printf("%i |%s|\n", Echo->i, str);
-	y = Echo->i;
-	while (str[Echo->i] != '\0')
+	i = 0;
+	i = skip_character(str, i, ' ');
+	if (str[i] == '-')
 	{
-		len++;
-		Echo->i++;
-	}
-	tmp = (char *)malloc((len + 1) * sizeof(char));
-	len = 0;
-	if (tmp != NULL)
-	{
-		while (str[y])
+		while (str[i] == '-')
 		{
-			tmp[len] = str[y];
-			y++;
-			len++;
+			i = ft_strncmp_echo(i, str, "-n", 2);
+			if (str[i] != ' '  && str[i] != '\n')
+			{
+				return (i - 2);
+			}
+			while (str[i] == ' ')
+				i++;
 		}
-		Echo->cache = ft_strjoin(Echo->cache, tmp);
-		printf("cache = %s\n", Echo->cache);
+		return (i);
 	}
-		free(tmp);
+	return (i);
 }
 
-void        set_value(t_struct_m *Echo)
+void        set_value(t_struct_m *echo)
 {
-	Echo->cache = ft_strdup("");
-	Echo->i       = 0;
-	Echo->path    = 0;
-	Echo->single  = 0;
-	Echo->Double  = 0;
-	Echo->end     = 0;
-	Echo->len     = 0;
-	Echo->stat    = 0;
+	echo->cache 	= ft_strdup("");
+	// echo->tmp		= NULL;
+	echo->str		= NULL;
+	echo->i       	= 0;
+	echo->path    	= 0;
+	echo->single  	= 0;
+	echo->doubble 	= 0;
+	echo->beg		= -1;
+	echo->end     	= 0;
+	echo->len     	= 0;
+	echo->stat    	= 0;
+	echo->n			= -1;
+	echo->error		= 0;
 }
+
+int		skip_character(char *str, int i, char c)
+{
+	while (str[i] == c && str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void		check_empty_beg(t_struct_m *echo)
+{
+	int i;
+
+	i = echo->i;
+	if (echo->beg == -1)
+		echo->beg = 0;
+	i--;
+	if (echo->str[i] == ' ')
+		echo->beg = 1;
+	else
+		echo->beg = 0;
+}
+
+char*	ft_fillstr(char *str1, char	c, int len)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+	{
+		str1[i] = c;
+		i++;
+	}
+	str1[i] = '\0';
+	return (str1);
+}
+
+void	echo_no_str(t_struct_m *echo)
+{
+	int		z = 0;
+	int		y   =	echo->i;
+	int     len =   0;
+	char*   tmp = 	NULL;
+
+	while (echo->str[echo->i] != '\0' && echo->str[echo->i] != '\n' &&
+		echo->str[echo->i] != ' ' && echo->str[echo->i] != '\"' && echo->str[echo->i] != '\'')
+		echo->i++;
+	len = echo->i - y;
+	tmp = (char *)malloc((len + 1) * sizeof(char));
+	if (tmp == NULL)
+		echo->error = -1;
+	else
+	{
+		tmp = ft_fillstr(tmp, '0', len);
+		while (len >= 0)
+		{
+			tmp[z] = echo->str[y];
+			z++;
+			y++;
+			len--;
+		}
+		echo->cache = ft_strjoin(echo->cache, tmp);
+		if (echo->beg == 1)
+		{
+			echo->cache = ft_strjoin(" ", echo->cache);
+			echo->beg = 0;
+		}
+		if (echo->str[echo->i + 1] == ' ')
+			echo->cache = ft_strjoin(echo->cache, " ");
+		free(tmp);
+	}
+}
+
 char       *echo_main(char  *str)
 {
-	t_struct_m *Echo;
+	t_struct_m *echo;
 
-	Echo = ft_calloc(1, sizeof(t_struct_m));
-
-	set_value(Echo);
-	Echo->end = check_n(str);
-	if (Echo->end == 1)
-		Echo->i = lenght_n(str); /*now it knows when -n has ended */
-	while (str[Echo->i] == ' ')
-		Echo->i++;
-	while (str[Echo->i])
+	echo = ft_calloc(1, sizeof(t_struct_m));
+	if (echo == NULL)
+		return (NULL);
+	set_value(echo);
+	echo->str = ft_strdup(str);
+	if (echo->str[0] == '\n')
+		return (echo->str);
+	echo->i = check_flag_n(str);/*now it knows when -n has ended */
+	echo->i = skip_character(echo->str, echo->i, ' ');
+	while (echo->str[echo->i] != '\n' && echo->str[echo->i] != '\0')
 	{
-		if (str[Echo->i] != '\"' || str[Echo->i] != '\'')
-			no_str(Echo, str);
+		check_empty_beg(echo);//at the start it will be
+		echo->i = skip_character(echo->str, echo->i, ' ');
+		if (echo->str[echo->i] != '\"' || echo->str[echo->i] != '\'')
+			echo_no_str(echo);
+		// else if (echo->str[echo->i] == '\'')
+		// 	echo_str_single(echo);
+		// else if (echo->str[echo->i] != '\"')
+		// 	echo_str_double(echo);
+		if (echo->error == -1)
+			return (ft_strdup(""));
+		//get in, if double or if single isn't a pair, then return a empty cache,
+		//and put error on and return the cache immidiently
 	}
-	return (Echo->cache);
+	if (echo->n != -1)
+		echo->cache = ft_strjoin(echo->cache, "\n");
+	printf("cache end = %s\n", echo->cache);
+	return (echo->cache);
 }
-
