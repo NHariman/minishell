c7ce04c1480f66d3be3b33723a61ef946a53750f
Yusha <yushabakker1@gmail.com>
2020-11-15 19:23:35 +0100

    sytax > error

--- srcs/rd/re_direct.c
+++ srcs/rd/re_direct.c
@@ -6,7 +6,7 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/12 15:28:15 by ybakker       #+#    #+#                 */
-/*   Updated: 2020/11/15 18:52:47 by ybakker       ########   odam.nl         */
+/*   Updated: 2020/11/15 19:23:21 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,29 @@
 #include <stdio.h>
 
 /*
-	first check for >
-
-	if file is there, chck if its ther,e then jsut put the info in there
-	//if file is not there, create and then ut it in
-
-	go to the back until you see a > if there is a " " then put that on and move
-	until you see another one that hasn't gotten a /, if you find a , that doens't have double or
-	single on, then you can save this as the rdi->i and use begin, and then the first string
-	you get until empty line or '\0' is the file name, if there is no '0'then the rest is a string you put in
-	str. now you move along the logic
-
-	frist string is file
-	second string is word until <
-	save this string and put it through the funnel
-	keep saving the tring accordingly
-	> means after eah other
-	>> means add a \n and then the word
-
-	>> means you add the whole str that is created, fi ther eis already one, then do it under
-
-	echo hello  >> file1 pwd >> file2 hello >>file3
-	if echo, then the str are str
-	else, for example pwd, you only hav the input pwd
-
 	action txt > file txt
 
     error
 
     3 = bash: syntax error near unexpected token `>' 
     4 = bash: syntax error near unexpected token `>>'
+
+    syntax error, so too many >>> first check before going in.
 */
+// int         error_sytax(char *str)
+// {
+//     //put this in a check before
+//     // if (rd->str[rd->i] == '>' && rd->str[rd->i + 1] == '>')
+//     // {
+//     //     error = 4;
+//     //     return (4);
+//     // }
+//     // else if (rd->str[rd->i] == '>')
+//     // {
+//     //     error = 3;
+//     //     return (3);
+//     // }
+// }
 
 int         rd_check_error_rd(t_struct_rd *rd)
 {
@@ -57,16 +49,6 @@ int         rd_check_error_rd(t_struct_rd *rd)
     if (rd->str[rd->i] == '>')
         rd->dir++;
     rd->i++;
-    if (rd->str[rd->i] == '>' && rd->str[rd->i + 1] == '>')
-    {
-        error = 4;
-        return (4);
-    }
-    else if (rd->str[rd->i] == '>')
-    {
-        error = 3;
-        return (3);
-    }
     return (0);
 }
 
@@ -128,7 +110,8 @@ int			rd_loop(t_struct_rd *rd, t_shell *shell)
     echo = ft_calloc(1, sizeof(t_struct_m));
     rd->i = 0;
     rd->file = ft_strdup("");
-    while (rd->str[rd->i] || rd->str[rd->i] == '\n')
+    // error = ft_check_sytax(rd->str);
+    while (rd->str[rd->i] || rd->str[rd->i] == '\n' && error = 0)
     {
         rd->dir = 0;
         error = rd_check_error_rd(rd);
