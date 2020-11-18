51e9c361f4fbcd8aa9b58d80b38198b2a9e8d06a
Yushua <yushabakker1@gmail.com>
2020-11-18 12:14:32 +0100

    push

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
@@ -43,3 +44,50 @@ int		main(int argc, char *argv[], char *envp[])
 	}
 	return (0);
 }
+=======
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/31 15:07:26 by ybakker       #+#    #+#                 */
+/*   Updated: 2020/11/15 19:10:50 by ybakker       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "srcs/minishell.h"
+#include <stdio.h>
+
+int		main(int argc, char *argv[], char *envp[])
+{
+	int			i;
+	char		*line;
+	char		**hold;
+	char		*str;
+	t_shell		*shell;
+	// t_struct_m	*echo;
+	t_struct_rd	*rd;
+
+	i = argc;
+	hold = argv;
+	shell = calloc(1, sizeof(t_shell));
+	rd = calloc(1, sizeof(t_struct_m));
+	shell->env = envp;
+	while (i > 0)
+	{
+		ft_printf("\033[1;36m");
+		ft_printf("minishell> ");
+		ft_printf("\033[0m");
+		i = get_next_line(0, &line);
+		if (!ft_strncmp(line, "exit\n", ft_strlen("exit\n")))
+			exit(0);
+		str = rd_main(line, rd, shell);
+		//minishell_parser(line, envp);
+		// free(line);//double free???
+		free(str);
+	}
+	return (0);
+}
+>>>>>>> Stashed changes
