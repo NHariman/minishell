03e17a9a108283a3f00388aed34cb13cb4e3da16
Yushua <yushabakker1@gmail.com>
2020-11-23 20:57:02 +0100

    makes files

--- srcs/rd/make_files.c
+++ srcs/rd/make_files.c
@@ -39,17 +39,36 @@ static int		open_close_file(char *str, int nb, t_shell *shell)
 		shell->exit_code = 1;
 		return (1);
 	}
-	if (nb == 1)
-	{
-		return (0);
-	}
-	else if (nb == 2)
-	{
-		return (0);
-	}
+	close(fd);
+	nb = 0;
 	return (0);
 }
 
+static int		open_fill_close_file(char *str, int nb, t_shell *shell)
+{
+	int		fd;
+
+	errno = 0;
+	fd = open(str, O_EXCL | O_CREAT, 0666);
+	if (fd < 0)
+	{
+		ft_printf_err("Error\n%s\n", strerror(errno));
+		shell->exit_code = 1;
+		return (1);
+	}
+	// if (nb == 1)
+	// {
+	// 	return (0);
+	// }
+	// if (nb == 2)
+	// {
+		//ft_printf("", fd)
+	// 	return (0);
+	// }
+	nb = 0;
+	close(fd);//if shell is on, do this after shell is done
+	return (0);
+}
 void	ft_rd_output(char *str, t_shell *shell)
 {
 	int		i;
@@ -64,10 +83,19 @@ void	ft_rd_output(char *str, t_shell *shell)
 		ft_printf("string ==: {%s}\n", string);
 		while (str[i] != '\n')
 			i++;
-		i++;
-		nb = ft_c_atot(str[i]);
-		i++;
+		i++;//after single /n
+		nb = ft_c_atot(str[i]);//get dir
+		i++;//after /n
 		if (open_close_file(string, nb, shell) == 1)
+		{
+			free(string);
+			return ;
+		}
+		free(string);
+	}
+	if (str[i] == '\n' && str[i + 1] == '\n')
+	{
+		if(open_fill_close_file(string, nb, shell) == 1)
 			return ;
 	}
 }
