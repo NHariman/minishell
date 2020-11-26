ecfe784861f7d54c1fb85903e9af147c260c0a95
Yusha <yushabakker1@gmail.com>
2020-11-26 13:21:16 +0100

    push

--- srcs/main/rd_parser.c
+++ srcs/main/rd_parser.c
@@ -6,13 +6,13 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/15 18:26:39 by nhariman      #+#    #+#                 */
-/*   Updated: 2020/11/26 13:06:28 by nhariman      ########   odam.nl         */
+/*   Updated: 2020/11/26 13:13:27 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	ft_qt_line(char *line, t_qts *qts, int *i)
+static void	ft_qt_rd(char *line, t_qts *qts, int *i)
 {
 	while (line[*i] != '\0')
 	{
