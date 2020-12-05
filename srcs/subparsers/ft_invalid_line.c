d524fddbf75a7031b1479d8de72ef9d822a881fd
Niks Hariman <nhariman@hotmail.com>
2020-12-05 19:26:22 +0100

    added start to line checker for semicolon validity

--- srcs/subparsers/ft_invalid_line.c
+++ srcs/subparsers/ft_invalid_line.c
@@ -6,13 +6,23 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/03 19:44:11 by nhariman      #+#    #+#                 */
-/*   Updated: 2020/12/05 18:18:38 by nhariman      ########   odam.nl         */
+/*   Updated: 2020/12/05 19:11:34 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 static void	ft_find_input(char **tmp, char *str, int *i, t_shell *shell)
+{
+	*tmp = ft_no_quotes_str(str, i, shell, " ;");
+	*i = *i + ft_iswhitespaces(str + *i);
+	if (str[*i] == ';' && str[*i + ft_iswhitespaces(str + *i + 1)] != '\0')
+	{
+		*i = *i + 1;
+		*tmp = NULL;
+		free(*tmp);
+	}
+}
 
 int			ft_invalid_line(char *str, t_shell *shell)
 {
@@ -31,16 +41,7 @@ int			ft_invalid_line(char *str, t_shell *shell)
 		}
 		else if (str[i] != ' ' &&
 			str[i + 1 + ft_iswhitespaces(str + i + 1)] != '\0' && str[i] != ';')
-		{
-			tmp = ft_no_quotes_str(str, &i, shell, " ;");
-			i = i + ft_iswhitespaces(str + i);
-			if (str[i] == ';' && str[i + ft_iswhitespaces(str + i + 1)] != '\0')
-			{
-				i++;
-				tmp = NULL;
-				free(tmp);
-			}
-		}
+			ft_find_input(&tmp, str, &i, shell);
 		else
 			i++;
 	}
