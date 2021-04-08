29efa711934b85acd6a8a42bf796a6755761b57c
Yusha <yushabakker1@gmail.com>
2021-04-08 17:04:19 +0200

    fixed norm

--- srcs/syntax/syntax_full_check.c
+++ srcs/syntax/syntax_full_check.c
@@ -6,24 +6,17 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/08 14:57:02 by ybakker       #+#    #+#                 */
-/*   Updated: 2021/04/08 16:55:58 by ybakker       ########   odam.nl         */
+/*   Updated: 2021/04/08 16:59:33 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	check_rd_newline(char *line, int i, char c)
+static int	syntax_backslash(void)
 {
-	while (line[i] == c)
-		i++;
-	if (line[i] == '\0')
-	{
-		ft_printf_err("minishell: syntax error ");
-		ft_printf_err("near unexpected token `newline'\n");
-		g_shell.exit_code = 258;
-		return (-1);
-	}
-	return (0);
+	ft_printf_err("minishell: syntax error near unexpected token '\\\'\n");
+	g_shell.exit_code = 258;
+	return (-1);
 }
 
 static int	is_token_check(char c, char *line, int i)
@@ -49,11 +42,7 @@ static int	is_token_check(char c, char *line, int i)
 			return (-1);
 	}
 	else if (line[i] == '\\')
-	{
-		ft_printf_err("minishell: syntax error near unexpected token '\\\'\n");
-		g_shell.exit_code = 258;
-		return (-1);
-	}
+		return (syntax_backslash());
 	return (0);
 }
 
