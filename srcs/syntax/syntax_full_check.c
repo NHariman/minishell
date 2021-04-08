9d556e8845d44716e2c18ad067c0718d857ad0f9
Yusha <yushabakker1@gmail.com>
2021-04-08 16:49:45 +0200

    IT.....WORK?

--- srcs/syntax/syntax_full_check.c
+++ srcs/syntax/syntax_full_check.c
@@ -6,12 +6,25 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/08 14:57:02 by ybakker       #+#    #+#                 */
-/*   Updated: 2021/04/08 15:58:05 by ybakker       ########   odam.nl         */
+/*   Updated: 2021/04/08 16:45:30 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static int	check_rd_newline(char *line, int i, char c)
+{
+    while (line[i] == c)
+        i++;
+    if (line[i] == '\0')
+    {
+        ft_printf_err("minishell: syntax error ");
+		ft_printf_err("near unexpected token `newline'\n");
+		g_shell.exit_code = 258;
+        return (-1);
+    }
+    return (0);
+}
 static int	is_token_check(char c, char *line, int i)
 {
 	if (c == '|')
@@ -26,12 +39,12 @@ static int	is_token_check(char c, char *line, int i)
     }
     else if (c == '>')
     {
-        if (check_rd_right(line, i))
+        if (check_rd_right(line, i) || check_rd_newline(line, i, '>'))
             return (-1);
     }
     else if (c == '<')
     {
-        if (check_rd_left(line, i))
+        if (check_rd_left(line, i) || check_rd_newline(line, i, '<'))
             return (-1);
     }
 	else if (line[i] == '\\')
