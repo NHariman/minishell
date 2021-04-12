d1436ab38b8c06218f714f4f046a20dca27c37b1
Niks Hariman <nhariman@hotmail.com>
2021-04-12 18:06:25 +0200

    badly fixed the rd

--- srcs/rd/rd_fill_file.c
+++ srcs/rd/rd_fill_file.c
@@ -6,14 +6,64 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/01 14:42:48 by ybakker       #+#    #+#                 */
-/*   Updated: 2021/04/01 14:43:32 by ybakker       ########   odam.nl         */
+/*   Updated: 2021/04/12 18:04:41 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 #include <stdio.h>
 
-static void	rd_one(t_struct_rd *rd)
+// static void	rd_one(t_struct_rd *rd)
+// {
+// 	pid_t	child_pid;
+// 	pid_t	tpid;
+// 	int		child_status;
+
+// 	tpid = 0;
+// 	child_pid = fork();
+// 	if (child_pid == 0)
+// 	{
+// 		dup2(rd->fd, 1);
+// 		ft_wordparser();
+// 		close(1);
+// 		close(rd->fd);
+// 		exit(0);
+// 	}
+// 	else
+// 	{
+// 		while (tpid != child_pid)
+// 		{
+// 			tpid = wait(&child_status);
+// 		}
+// 	}
+// }
+
+// static void	rd_two(t_struct_rd *rd)
+// {
+// 	pid_t	child_pid;
+// 	pid_t	tpid;
+// 	int		child_status;
+
+// 	tpid = 0;
+// 	child_pid = fork();
+// 	if (child_pid == 0)
+// 	{
+// 		dup2(rd->fd_rd, 0);
+// 		ft_wordparser();
+// 		close(0);
+// 		close(rd->fd_rd);
+// 		exit(0);
+// 	}
+// 	else
+// 	{
+// 		while (tpid != child_pid)
+// 		{
+// 			tpid = wait(&child_status);
+// 		}
+// 	}
+// }
+
+static void	do_rd(t_struct_rd *rd)
 {
 	pid_t	child_pid;
 	pid_t	tpid;
@@ -23,35 +73,21 @@ static void	rd_one(t_struct_rd *rd)
 	child_pid = fork();
 	if (child_pid == 0)
 	{
-		dup2(rd->fd, 1);
+		if (rd->fd != -1)
+			dup2(rd->fd, 1);
+		if (rd->fd_rd != -1)
+			dup2(rd->fd_rd, 0);
 		ft_wordparser();
-		close(1);
-		close(rd->fd);
-		exit(0);
-	}
-	else
-	{
-		while (tpid != child_pid)
+		if (rd->fd_rd != -1)
 		{
-			tpid = wait(&child_status);
+			close(0);
+			close(rd->fd_rd);
 		}
-	}
-}
-
-static void	rd_two(t_struct_rd *rd)
-{
-	pid_t	child_pid;
-	pid_t	tpid;
-	int		child_status;
-
-	tpid = 0;
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-		dup2(rd->fd_rd, 0);
-		ft_wordparser();
-		close(0);
-		close(rd->fd_rd);
+		if (rd->fd != -1)
+		{
+			close(1);
+			close(rd->fd);
+		}
 		exit(0);
 	}
 	else
@@ -65,14 +101,22 @@ static void	rd_two(t_struct_rd *rd)
 
 void	rd_open_file_fill(t_struct_rd *rd)
 {
-	if (rd->fd != -1)
+	if (rd->fd != -1 || rd->fd_rd != -1)
 	{
-		rd_one(rd);
-		close(rd->fd);
+		do_rd(rd);
+		if (rd->fd != -1)
+			close(rd->fd);
+		if (rd->fd_rd != -1)
+			close(rd->fd_rd);
 	}
-	else if (rd->fd_rd != -1)
-	{
-		rd_two(rd);
-		close(rd->fd_rd);
-	}
+	// if (rd->fd != -1)
+	// {
+	// 	rd_one(rd);
+	// 	close(rd->fd);
+	// }
+	// else if (rd->fd_rd != -1)
+	// {
+	// 	rd_two(rd);
+	// 	close(rd->fd_rd);
+	// }
 }
--- srcs/rd/rd_loop.c
+++ srcs/rd/rd_loop.c
@@ -6,7 +6,7 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/01 14:48:53 by ybakker       #+#    #+#                 */
-/*   Updated: 2021/04/08 08:45:41 by ybakker       ########   odam.nl         */
+/*   Updated: 2021/04/12 17:51:44 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
