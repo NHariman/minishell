1e194fb855f933643fdeaf62e268107ed3096538
Yusha <yushabakker1@gmail.com>
2021-04-15 17:54:06 +0200

    pushing

--- srcs/rd/rd_open_file.c
+++ srcs/rd/rd_open_file.c
@@ -6,7 +6,7 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/01 14:42:14 by ybakker       #+#    #+#                 */
-/*   Updated: 2021/04/15 14:02:30 by nhariman      ########   odam.nl         */
+/*   Updated: 2021/04/15 17:17:58 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,39 +32,48 @@ static void	rd_file_error(t_struct_rd *rd)
 
 void	rd_open_file(t_struct_rd *rd)
 {
-	errno = 0;
-	int		fd;
-	
-	fd = -1;
 	if (rd->out != -1 && (rd->nb == 1 || rd->nb == 2))
 	{
-		ft_printf("close fd\n");
+		ft_printf("close OUT\n");
 		close(rd->out);
+		rd->out = -1;
+		ft_printf("error OUT [%i] read\n", read(rd->out, NULL, 0));
+		ft_printf("error OUT [%i] fd\n", rd->out);
 	}
 	else if (rd->in != -1 && rd->nb == 3)
 	{
-		ft_printf("close fd_rd\n");
+		ft_printf("close IN\n");
 		close(rd->in);
+		rd->in = -1;
+		ft_printf("error IN [%i] read\n", read(rd->in, NULL, 0));
+		ft_printf("error IN [%i] fd\n",  rd->in);
 	}
 	if (rd->nb == 1)
 	{
-		ft_printf("filename out = [%s]\n", rd->file);
 		rd->out = open(rd->file, O_RDWR | O_TRUNC | O_CREAT, 0666);
-		ft_printf("fd = [%i]\n", rd->out);
+		ft_printf("OUT [%i]\n",  rd->out);
+		ft_printf_err("out: %s: %s\n", rd->file, strerror(errno));
 	}
 	else if (rd->nb == 2)
 	{
-		ft_printf("filename out = [%s]\n", rd->file);
 		rd->out = open(rd->file, O_RDWR | O_APPEND | O_CREAT, 0666);
-		ft_printf("fd = [%i]\n", rd->out);
+		ft_printf("OUT [%i]\n",  rd->out);
+		ft_printf_err("out: %s: %s\n", rd->file, strerror(errno));	
 	}
 	else if (rd->nb == 3)
 	{
-		ft_printf("filename in = [%s]\n", rd->file);
 		rd->in = open(rd->file, O_RDWR);
-		ft_printf("fd_rd = [%i]\n", rd->in);
+		ft_printf("IN = [%i]\n",  rd->in);
+		ft_printf_err("in: %s: %s\n", rd->file, strerror(errno));
 	}
 	if (rd->error != 1)
 		rd_file_error(rd);
+	ft_printf("IN = [%i]\n",  rd->in);
+	ft_printf("IN = [%i]\n", read(rd->in, NULL, 0));
+	ft_printf_err("%s\n", strerror(errno));
+	errno = 0;
+	ft_printf("OUT [%i]\n", read(rd->out, NULL, 0));
+	ft_printf("OUT [%i]\n",  rd->out);
+	ft_printf_err("%s\n", strerror(errno)); 
 	ft_printf("---DONE2---\n");
 }
