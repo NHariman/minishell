53842ad997832100dc4a440329332f30b36d9465
Yushua <yushabakker1@gmail.com>
2021-04-16 13:37:36 +0200

    spliting open

--- srcs/rd/rd_open_file.c
+++ srcs/rd/rd_open_file.c
@@ -13,20 +13,29 @@
 #include "../minishell.h"
 #include <stdio.h>
 
-static void	rd_file_error(t_struct_rd *rd)
+static void	rd_open_out(t_struct_rd *rd, int flag)
 {
-	if (rd->out < 0 && rd->in < 0)
+	if (rd->out != -1)
+		close(rd->out);
+	rd->out = open(rd->file, O_CREAT | flag | O_WRONLY, 0644);
+	if (rd->out == -1)
 	{
 		ft_printf_err("minishell: %s: %s\n", rd->file, strerror(errno));
 		g_shell.exit_code = 1;
 		rd->error = 1;
 	}
-	else if (rd->str[rd->i] && rd->str[rd->i] != '\n' && rd->error == 0)
+}
+
+tatic void	rd_open_in(t_struct_rd *rd)
+{
+	if (rd->in != -1)
+		close(rd->in);
+	rd->in = open(rd->file, O_RDWR, 0644);
+	if (rd->in == -1)
 	{
-		if (rd->nb == 1 || rd->nb == 2)
-			close(rd->out);
-		else if (rd->nb == 3)
-			close(rd->in);
+		ft_printf_err("minishell: %s: %s\n", rd->file, strerror(errno));
+		g_shell.exit_code = 1;
+		rd->error = 1;
 	}
 }
 
@@ -34,24 +43,12 @@ void	rd_open_file(t_struct_rd *rd)
 {
 	errno = 0;
 	open_check();
-	if (rd->out != -1 && (rd->nb == 1 || rd->nb == 2))
-	{
-		ft_printf("close fd\n");
-		close(rd->out);
-	}
-	else if (rd->in != -1 && rd->nb == 3)
-	{
-		ft_printf("close fd_rd\n");
-		close(rd->in);
-	}
 	if (rd->nb == 1)
-		rd->out = open(rd->file, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		rd_open_out(rd, O_TRUNC);
 	else if (rd->nb == 2)
-		rd->out = open(rd->file, O_CREAT | O_APPEND | O_WRONLY, 0644);
+		rd_open_out(rd, O_APPEND);
 	else if (rd->nb == 3)
-		rd->in = open(rd->file, O_RDWR, 0644);
-	if (rd->error != 1)
-		rd_file_error(rd);
+		rd_open_in(rd);
 	ft_printf("IN = [%i] OUT = [%i]\n", rd->in, rd->out);
 	ft_printf("IN = [%i] OUT = [%i]\n", read(rd->in, NULL, 0), read(rd->out, NULL, 0));
 	ft_printf("---DONE2---\n");
