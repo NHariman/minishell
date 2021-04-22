112ec5ab7869d9b39bc7b7f444ba64023577804e
Yusha <yushabakker1@gmail.com>
2021-04-22 11:26:41 +0200

    making it beautifull

--- srcs/syntax/syntax_pipes.c
+++ srcs/syntax/syntax_pipes.c
@@ -6,7 +6,7 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/08 19:28:56 by ybakker       #+#    #+#                 */
-/*   Updated: 2021/04/22 11:24:39 by ybakker       ########   odam.nl         */
+/*   Updated: 2021/04/22 11:26:33 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,8 @@ int syntax_pipes(char *line, int i)
         return (syntax_multiline_error());
     else if (line[start] == '|')
         return (syntax_pipe_error());
-    // ft_printf("i == [%i] c == [%c]\n", start, line[start]);
     while (line[start] == ' ' || line[start] == '<' || line[start] == '>')
-    {
         start++;
-    }
-    // ft_printf("i == [%i] c == [%c]\n", start, line[start]);
     if (line[start] == '|')
         return (syntax_pipe_error());
     return (0);
